608f0c6406c6057c6e1cfeaf6b9197b9234b36a2
Reece Dunham <me@rdil.rocks>
2020-04-06 11:53:52 -0400

    cleanup

--- snowfall.js
+++ snowfall.js
@@ -1,7 +1,9 @@
-$(init)
+$(init);
+
 function randInt(min, max) {
   return min + Math.floor(Math.random() * (max - min))
 }
+
 var frame = 0;
 class Snowflake {
   constructor(x, y) {
@@ -9,32 +11,33 @@ class Snowflake {
     this.y = y;
   }
   draw() {
-    $("body").append(`<svg style="position: absolute; top: ${this.y}px; left: ${this.x}px;" >
-      <circle cx="5" cy="5" r="5" fill="white"/>
+    $("body").append(`<svg style="position: absolute; top: ${this.y}px; left: ${this.x}px;">
+      <circle cx="5" cy="5" r="5" fill="white" />
       </svg>`)
     this.y += 3;
   }
 }
-snowflakes = []
 
-function spawnSnowflakes(){
-  if (frame % 25 === 0){
-    snowflakes.push(new Snowflake(randInt(0, window.innerWidth-50), 0))
+snowflakes = [];
+
+function spawnSnowflakes() {
+  if (frame % 25 === 0) {
+    snowflakes.push(new Snowflake(randInt(0, window.innerWidth-50), 0));
   }
 }
+
 function init() {
-  var snowstorm = setInterval(() => {
+  var snowstorm = setInterval(function() {
     spawnSnowflakes();
     $('svg').remove();
-    snowflakes.forEach(snowflake => {
+    snowflakes.forEach(function(snowflake) {
       snowflake.draw();
     })
-    for(var i = 0; i < snowflakes.length; i++){
-      if(snowflakes[i].y>$("body").height()-10){
-        snowflakes.splice(i, 1)
+    for (var i = 0; i < snowflakes.length; i++) {
+      if (snowflakes[i].y > $("body").height() - 10) {
+        snowflakes.splice(i, 1);
       }
     }
-    frame+=1;
-    console.log(frame);
-  }, 5)
+    frame += 1;
+  }, 5);
 }
